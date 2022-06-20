5a7b4d6201eedf665d8be780f8363618f9d12012
ev2li <ev2li9748@163.com>
2022-06-20 22:22:05 +0800

    log

--- sylar/log.cc
+++ sylar/log.cc
@@ -88,11 +88,30 @@ std::string LogFormatter::format(LogEvent::ptr event){
 }
 
 void LogFormatter::init(){
-    std::vector<std::pair<std::string, int>> vec;
-    size_t last_pos = 0;
+    //str,  format, type
+    std::vector<std::tuple<std::string,  std::string, int>> vec;
+    std::string str;
     for(size_t i = 0; i < m_pattern.size(); ++i){
-        if(m_pattern[i] == "%"){
-            
+        if(m_pattern[i] == '%'){
+            str.append(1, m_pattern[i]);
+            continue;
+        }
+
+        size_t a = i + 1;
+        int fmt_status = 0;
+        std::string str;
+        std::string fmt;
+        while(n < m_pattern.size()){
+            if(isspace(m_pattern[n])){
+                break;
+            }
+            if(fmt_status == 0){
+                if (m_pattern[n] == "["){
+                    str = m_pattern.substr(i + 1, n - i);
+                    
+                }
+            }
+
         }
     }
 }
