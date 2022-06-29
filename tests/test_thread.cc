c670a15356fb2c505b5ababf16399710785615b0
ev2li <ev2li9748@163.com>
2022-06-29 15:49:47 +0800

    mutex

--- tests/test_thread.cc
+++ tests/test_thread.cc
@@ -61,5 +61,12 @@ int main(int argc, char const *argv[])
 
     SYLAR_LOG_INFO(g_logger) << "thread test end";
     SYLAR_LOG_INFO(g_logger) << "count" << count;
+
+    sylar::Config::Visit([](sylar::ConfigVarBase::ptr var){
+        SYLAR_LOG_INFO(g_logger) << "name = " << var->getName()
+                    << " description = " << var->getDescription()
+                    << " typename = " << var->getTypeName()
+                    << " value =  " <<  var->toString();
+    });
     return 0;
 }
